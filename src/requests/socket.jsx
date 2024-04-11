23ee2602d504d9c0c89df2dc8e6d1a4fd47003a6
chunkie <gsc0518@outlook.com>
2024-04-11 22:12:36 +1000

    11 Apr 2024

--- src/requests/socket.jsx
+++ src/requests/socket.jsx
@@ -1,6 +1,6 @@
 import { Client } from "@stomp/stompjs";
 
-const brokerURL = "ws://" + process.env.REACT_APP_ENDPOINT + "/ws";
+const brokerURL = "wss://" + process.env.REACT_APP_ENDPOINT + "/ws";
 
 const client = new Client({
   brokerURL: brokerURL,

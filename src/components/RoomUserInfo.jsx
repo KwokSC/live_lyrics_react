2376ca3af8b59d870f3685db4dd8abf6ab48a386
chunkie <gsc0518@outlook.com>
2024-04-16 16:26:33 +1000

    16 Apr 2024

--- src/components/RoomUserInfo.jsx
+++ src/components/RoomUserInfo.jsx
@@ -1,6 +1,6 @@
 export default function RoomUserInfo({ user }) {
   return (
-    <div className="room-user-info">
+    <div className={`room-user-info ${user.type === "HOST" ? "host" : ""}`}>
       <div className="profile-img">
         {user.profileImg ? (
           <img src={user.profileImg} />
@@ -8,8 +8,10 @@ export default function RoomUserInfo({ user }) {
           <i className="fi fi-rr-circle-user" />
         )}
       </div>
-      <p style={{fontSize:"15px"}}>{user.userName}</p>
-      <p style={{fontSize:"10px",fontStyle:"italic", marginTop:"3px"}}>{"@"+user.userAccount}</p>
+      <p style={{ fontSize: "12px" }}>{user.userName}</p>
+      <p style={{ fontSize: "10px", fontStyle: "italic", marginTop: "3px" }}>
+        {"@" + user.userAccount}
+      </p>
     </div>
   );
 }
